377fa45938cac0687bacebb6b621514255174256
Stefan Höhn <webmaster@untergang.de>
2016-12-26 01:07:46 +0100

    Update youtube-dl.sh

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -2,9 +2,9 @@
 
 #should be executed every day (crontab -e) 
 datum=$(date -d "1 day ago" '+%Y%m%d')
-#switch to the default firefox dir with sqlite databases
+#switch to the default firefox folder with sqlite databases
 cd ~/.mozilla/firefox/*default*
-# firefox manage bookmarks in a database. Firefox  bookmarks needs a directory auto-yt.
+#firefox manages bookmarks in a database. Firefox  bookmarks needs a folder auto-yt.
 #save  playlists in this directory
 dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title  == "auto-yt"))'; ))
 #switch  home
