353161a7d9eeddb3e8ff12f3c4134a63f9ba302e
dewomser <webmaster@untergang.de>
2017-01-31 17:40:42 +0100

    There is an option only array

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # FF-auto-youtube-download-
-This Bash-script needs a Firefox-Browser,youtube-dl and sqlite3 
+This Bash-script needs a Firefox-Browser, youtube-dl and sqlite3 
 The script downloads only fresh Videos to your harddisk.
 Source are the URLs from a Bookmarks folder
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,14 +1,30 @@
 #! /bin/bash
+#This Bash-script should be executed every day (crontab -e) 
 
-#should be executed every day (crontab -e) 
+#update youtube-dl
+youtube-dl -U
+sleep 5
+
+#Date= yesterday
 datum=$(date -d "1 day ago" '+%Y%m%d')
-#switch to the default firefox folder with sqlite databases
+
+# from array. No Firefox needed
+#dbarray=(
+#"https://www.youtube.com/user/example-foo"
+#"https://www.youtube.com/channel/example-bar/videos?sort=dd&shelf_id=0&view=0"
+#)
+
+# from Firefox bookmarks
+#switch to the default firefox folder with sqlite databases.
+#change *default* if you have other profiles than default.
 cd ~/.mozilla/firefox/*default*
-#firefox manages bookmarks in a database. Firefox  bookmarks needs a folder auto-yt.
-#save  playlists in this directory
+
+#firefox manages bookmarks from database. 
+#Firefox  bookmarks needs a folder auto-yt.
+#save all your Youtube playlists in auto-yt
 dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title  == "auto-yt"))'; ))
-#switch  home
-cd
+#switch  to your youtube download folder
+cd ~/Downloads
 #let youtube-dl do the work  and download brandnew videos
 for i in ${dbarray[@]}; do
 youtube-dl --dateafter $datum --playlist-end 4 --max-downloads 2 $i
