b5f739237b1b49d755ade3aa58bff999f4482922
dewomser <webmaster@untergang.de>
2018-05-22 10:27:47 +0200

    Minor Improvements, extern Downloader aria2 is an option, script is shellchecked, comment = ## example = #

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # FF-auto-youtube-download-
-This Bash-script needs a Firefox-Browser, youtube-dl and sqlite3 
-The script downloads only fresh Videos to your harddisk.
-Source are the URLs from a Bookmarks folder
+##Download favourite Videos from an array or Firefox-folder
+No need to login
+The script downloads only fresh Videos to disk.
+
+1.option array : only shell and youtube-dl is needed
+2.option firefox-folder : shell, firefox, sqlite3, (aria2)
+
+
+
--- youtube-dl.sh
+++ youtube-dl.sh
@@ -9,27 +9,28 @@
 # sleep 5
 
 # Variables #
-#firefox manages bookmarks from database. 
-#save all your Youtube playlists in $favdir
+##firefox manages bookmarks from database. ##
+##save all your Youtube playlists in $favdir##
 favdir="bestof"
 echo $favdir
-#Download folder
+##Download folder##
 dl_folder="/home/foo/Downloads/youtube-dl/"
 echo  $dl_folder
-#Date= yesterday
+##Date= yesterday##
 datum=$(date -d "1 day ago" '+%Y%m%d')
-# Videos per day in each playlist
+## Videos per day in each playlist##
 perday=4
-#if aria2 is installed example :--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"
+##if aria2 is installed example :##
+#aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
 aria2=''
 
-# from Firefox bookmarks
-# switch to the default firefox folder with sqlite databases.
-# change *default* if you have other profiles than default.
+## from Firefox bookmarks##
+## switch to the default firefox folder with sqlite databases.##
+## change *default* if you have other profiles than default.##
 cd ~/.mozilla/firefox/*default* || exit
 
-# uncomment if you  load from an array
-# No Firefox no variables needed 
+##uncomment if you  load from an array##
+##No Firefox no variables needed##
 #dl_folder="~/Downloads"
 #perday=4
 #dbarray=(
@@ -37,15 +38,15 @@ cd ~/.mozilla/firefox/*default* || exit
 #"https://www.youtube.com/channel/example-bar/videos?sort=dd&shelf_id=0&view=0"
 #)
 
-#comment this if you load from array
-#firefox manages bookmarks from database. 
-#Firefox  bookmarks needs a folder $favdir.
-#save all your Youtube playlists in $favdir
+##comment this if you load from array##
+##firefox manages bookmarks from database.## 
+##Firefox  bookmarks needs a folder $favdir.##
+##save all your Youtube playlists in $favdir##
 dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
 
-#this  must not be commented
+##this  must not be commented##
 cd  $dl_folder || exit
-# let youtube-dl do the work  and download brandnew videos
+## let youtube-dl do the work  and download brandnew videos##
 # 
 for i in "${dbarray[@]}"; do
 youtube-dl "$aria2" --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
