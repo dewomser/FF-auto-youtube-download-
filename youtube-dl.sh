d6817323102df7237f952904f4ae536ec294225f
dewomser <webmaster@untergang.de>
2018-05-22 09:41:49 +0200

    small improvements, aria2c build in , shellchecked

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,34 +1,53 @@
-#! /bin/bash
-#This Bash-script should be executed every day (crontab -e) 
+#!/bin/bash
+#
+# This Shellscript downloads all fresh videos from afirefox favorite folder.
+# sqlite3 and youtube-dl must  be installed
+# 
+#update youtube-dl
+# "youtube-dl -U" or "pip youtube-dl install"
+# pip install --upgrade youtube-dl
+# sleep 5
 
-update youtube-dl
-youtube-dl -U
-sleep 5
-# fav dir
-# put in your favorite path
-favdir="auto-yt"
-#echo $favdir
+# Variables #
+#firefox manages bookmarks from database. 
+#save all your Youtube playlists in $favdir
+favdir="gestof"
+echo $favdir
+#Download folder
+dl_folder="/home/foo/Downloads/youtube-dl/"
+echo  $dl_folder
 #Date= yesterday
 datum=$(date -d "1 day ago" '+%Y%m%d')
+# Videos per day in each playlist
+perday=4
 
-# from array. No Firefox needed
+# from Firefox bookmarks
+# switch to the default firefox folder with sqlite databases.
+# change *default* if you have other profiles than default.
+cd ~/.mozilla/firefox/*default* || exit
+
+# uncomment if you  load from an array
+# No Firefox no variables needed 
+#dl_folder="~/Downloads"
+#perday=4
 #dbarray=(
 #"https://www.youtube.com/user/example-foo"
 #"https://www.youtube.com/channel/example-bar/videos?sort=dd&shelf_id=0&view=0"
 #)
 
-# from Firefox bookmarks
-#switch to the default firefox folder with sqlite databases.
-#change *default* if you have other profiles than default.
-cd ~/.mozilla/firefox/*default*
-
+#comment this if you load from array
 #firefox manages bookmarks from database. 
-#Firefox  bookmarks needs a folder auto-yt.
-#save all your Youtube playlists in auto-yt
+#Firefox  bookmarks needs a folder $favdir.
+#save all your Youtube playlists in $favdir
 dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
-#switch  to your youtube download folder
-cd ~/Downloads
-#let youtube-dl do the work  and download brandnew videos
-for i in ${dbarray[@]}; do
-youtube-dl --dateafter $datum --playlist-end 4 --max-downloads 2 $i
+
+#this  must not be commented
+cd  $dl_folder || exit
+# let youtube-dl do the work  and download brandnew videos
+# 
+for i in "${dbarray[@]}"; do
+youtube-dl --external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M" --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
+# echo $i
 done
+
+#dolphin $dl_folder
