735ee71034c900bca304fe9008902551cc667146
dewomser <webmaster@untergang.de>
2018-10-30 10:27:15 +0100

    change source by variable: array or db

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 ## This Shellscript downloads all fresh videos from afirefox favorite folder. ##
 
+## Config ##
+
 ## update youtube-dl ##
 ## "youtube-dl -U" or "pip youtube-dl install" ##
 # pip install --upgrade youtube-dl
-# sleep 5
+# sleep 3
 
-## Variables ##
 ## Firefox manages bookmarks from database. ##
-
-# All firefox intances must be killed.
-# Reason is: Error: database is locked
-
+## All firefox instances must be killed.
+## Reason is: Error: database is locked
 killall -9 firefox; sleep 3
 
-# #save all your Youtube playlists in $favdir ##
+## from Firefox bookmarks ##
+## change *default* if you have other profiles than default. ##
+cd ~/.mozilla/firefox/*default* || exit
+
+## Variables ##
+
+## save all your Youtube playlists in $favdir ##
 favdir="bestof"
-echo $favdir
 ## Download folder ##
 dl_folder="/home/foo/Downloads/youtube-dl/"
-echo  $dl_folder
 ## Date= yesterday ##
 datum=$(date -d "1 day ago" '+%Y%m%d')
 ## Videos per day in each playlist##
@@ -27,26 +30,31 @@ perday=4
 ## if aria2 is installed example : ##
 # aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
 aria2=''
+## load from database or array
+loadfrom=database
 
-## from Firefox bookmarks ##
-## change *default* if you have other profiles than default. ##
-## comment if you load from array ##
-cd ~/.mozilla/firefox/*default* || exit
+## Start 
 
-## uncomment if you  load from an array ##
-## No Firefox no variables needed ##
-# dl_folder="~/Downloads"
-# perday=4
-# dbarray=(
-# "https://www.youtube.com/user/example-foo"
-# "https://www.youtube.com/channel/example-bar/videos?sort=dd&shelf_id=0&view=0"
-# )
+# echo $favdir
+# echo  $dl_folder
 
-## comment this if you load from array ##
-## this line puts FF favourites from sqlite3 to an array ##
-dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
+## only edit dbarray content if you want to use it
 
-## this  must not be commented ##
+if [ $loadfrom == array ]
+then
+ dbarray=(
+ "https://www.youtube.com/user/GalileoOffiziell/videos"
+ "https://www.youtube.com/user/BibisBeautyPalace/videos?sort=dd&shelf_id=1&view=0"
+ "https://www.youtube.com/channel/UC53bIpnef1pwAx69ERmmOLA"
+ )
+##
+##--------------------Do not edit below this line ! -----------------------
+##
+ 
+else
+ ## this line puts FF favourites from sqlite3 to an array ##
+ dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
+fi
 cd  $dl_folder || exit
 ## let youtube-dl do the work  and download brandnew videos##
 # 
@@ -55,5 +63,5 @@ youtube-dl $aria2 --dateafter "$datum" --playlist-end "$perday" --max-downloads 
 # echo $i
 done
 
-## optional ###
+## optional ##
 #dolphin $dl_folder
