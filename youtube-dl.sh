7a44b268f978762458631e90a29f7728e209cdff
dewomser <webmaster@untergang.de>
2018-10-30 22:16:42 +0100

    firefox auto detect and  sqlite management

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 ## This Shellscript downloads all fresh videos from afirefox favorite folder. ##
 
-## Config ##
-
-## update youtube-dl ##
-## "youtube-dl -U" or "pip youtube-dl install" ##
+## update your youtube-dl ##
+# "youtube-dl -U" 
+## or 
 # pip install --upgrade youtube-dl
 # sleep 3
-
-## If Firefox is already running AND Sqlite bookmarks are used, this script will not do without action: ##
-## Firefox manages bookmarks from a locked database.
-
-## 1.) All Firefox instances must be killed. Manualy or uncomment next Line
-# killall -9 firefox; sleep 3
-## OR
-## 2.) copy database and query there. Uncomment line 22 ; read line 60  
-
-## from Firefox bookmarks ##
-## change *default* if you have other profiles than default. ##
-cd ~/.mozilla/firefox/*default* || exit
-# cp places.sqlite places2.sqlite
 
 ## Variables ##
 
 ## save all your Youtube playlists in $favdir ##
 favdir="bestof"
+## firefox running ?
+ffon=0; ps -ef|grep firefox|grep -v grep && ffon=1
 ## Download folder ##
 dl_folder="/home/foo/Downloads/youtube-dl/"
 ## Date= yesterday ##
 datum=$(date -d "1 day ago" '+%Y%m%d')
-## Videos per day in each playlist##
+## Videos per day on each playlist##
 perday=4
 ## if aria2 is installed example : ##
 # aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
 aria2=''
 ## load from database or array
 loadfrom=database
+## firefox database
+sqltdata=places.sqlite
 
 ## Start ##
 
-# echo $favdir
-# echo  $dl_folder
+cd ~/.mozilla/firefox/*default* || exit
+
+if (( ffon == 1 && loadfrom == database ))
+then
+cp $sqltdata places2.sqlite
+sqltdata=places2.sqlite
+fi
 
 ## only edit dbarray content if you want to use it
 
@@ -56,13 +51,14 @@ then
 ##
  
 else
- ## this line puts FF favourites from sqlite3 to an array ##
- ## edit "places" to "places2 in next line"
- dbarray=( $(sqlite3 -list places.sqlite 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
+ ## This line puts FF favourites from sqlite3 to an array ##
+ ## Only if 2.) Next line edit "places" to "places2"
+ dbarray=( $(sqlite3 -list $sqltdata 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
 fi
 cd  $dl_folder || exit
-## let youtube-dl do the work  and download brandnew videos##
-# 
+
+## Let youtube-dl do the work  and download brandnew videos ##
+
 for i in "${dbarray[@]}"; do
 youtube-dl $aria2 --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
 # echo $i
