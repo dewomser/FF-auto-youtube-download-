8bd4b3c12672dd25a40c9029d7076f90da060369
dewomser <webmaster@untergang.de>
2018-10-30 22:40:25 +0100

    documentation and garbage collection

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-## This Shellscript downloads all fresh videos from afirefox favorite folder. ##
+## This Shellscript downloads all fresh videos from a firefox bookmark folder. ##
 
-## update your youtube-dl ##
+## Update your youtube-dl ! ##
 # "youtube-dl -U" 
 ## or 
 # pip install --upgrade youtube-dl
@@ -9,20 +9,20 @@
 
 ## Variables ##
 
-## save all your Youtube playlists in $favdir ##
+## Save all your Youtube playlists in favdir ##
 favdir="bestof"
-## firefox running ?
+## Firefox running ?
 ffon=0; ps -ef|grep firefox|grep -v grep && ffon=1
 ## Download folder ##
 dl_folder="/home/foo/Downloads/youtube-dl/"
 ## Date= yesterday ##
 datum=$(date -d "1 day ago" '+%Y%m%d')
-## Videos per day on each playlist##
+## Max. videos / datum to each playlist ##
 perday=4
 ## if aria2 is installed example : ##
 # aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
 aria2=''
-## load from database or array
+## load from "database" or "array".
 loadfrom=database
 ## firefox database
 sqltdata=places.sqlite
@@ -37,7 +37,7 @@ cp $sqltdata places2.sqlite
 sqltdata=places2.sqlite
 fi
 
-## only edit dbarray content if you want to use it
+## only edit dbarray test-content if you want to use it
 
 if [ $loadfrom == array ]
 then
@@ -51,8 +51,8 @@ then
 ##
  
 else
- ## This line puts FF favourites from sqlite3 to an array ##
- ## Only if 2.) Next line edit "places" to "places2"
+ ## This line puts FF bookmarks from sqlite3 to an array ##
+
  dbarray=( $(sqlite3 -list $sqltdata 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
 fi
 cd  $dl_folder || exit
