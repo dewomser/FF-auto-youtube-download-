7fa02ec1bb3d2705269f940a79d8ce36718de7c1
dewomser <webmaster@untergang.de>
2018-11-25 10:50:54 +0100

    Aria2 downloader can be used now.

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-## This Shellscript downloads all fresh videos from a firefox bookmark folder. ##
+
+## This Shellscript downloads all fresh videos from array or firefox bookmark folder with youtube playlists. ##
 
 ## Update your youtube-dl ! ##
 # "youtube-dl -U" 
@@ -8,67 +9,63 @@
 # sleep 3
 
 ## Variables ##
-
-## Save all your Youtube playlists in favdir ##
+## ----------important-------------
+## Download folder make it. mkdir ~/Downloads/youtube-dl ##
+dl_folder=~/Downloads/youtube-dl
+## Save all your Youtube playlists in FF bookmark (bestof) ##
 favdir="bestof"
+## ------------ may help----------
 ## Firefox running ?
-# ffon=0; ps -ef|grep firefox|grep -v grep && ffon=1
 ffon=0; pgrep firefox && ffon=1
 ## Firefox default Profil
+## only if you not using the default FF-profile, this line must be edited ## 
 ffprofile=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=//)
-## Download folder ##
-dl_folder=~/Downloads/youtube-dl
 ## Date= yesterday ##
 datum=$(date -d "1 day ago" '+%Y%m%d')
-## Max. videos / datum to each playlist ##
+## Max. videos download in each playlist ##
 perday=4
-## if aria2 is installed example : ##
-# aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
-aria2=''
-## load from "database" or "array".
+## if aria2 downloader is installed and you want to use it, change from 0 to 1  ##
+aria=0
+## load from "database" or "array". database == firefox SQLITE 
 loadfrom=database
 ## firefox database
 sqltdata=places.sqlite
 
 ## Start ##
 
-## only edit dbarray test-content if you want to use it
-
+##  Use array, edit dbarray for your needs else this is ignored. ##
 if [ $loadfrom == array ]
 then
  dbarray=(
  "https://www.youtube.com/user/GalileoOffiziell/videos"
  "https://www.youtube.com/user/BibisBeautyPalace/videos?sort=dd&shelf_id=1&view=0"
  "https://www.youtube.com/channel/UC53bIpnef1pwAx69ERmmOLA"
- )
-##
-##--------------------Do not edit below this line ! -----------------------
-##
- 
-else
-
-# cd ~/.mozilla/firefox/*default* || exit
-cd ~/.mozilla/firefox/$ffprofile || exit
-
-
-if [ $ffon == 1 ] && [ $loadfrom == database ]
-then
-cp $sqltdata places2.sqlite
-sqltdata=places2.sqlite
-fi
-
+##-----carefully edit below this line ! ---------
+ else
+ # cd ~/.mozilla/firefox/*default* || exit
+ cd ~/.mozilla/firefox/$ffprofile || exit
+ ## If Firefox is running, db is locked. need a copy ##
+ if [ $ffon == 1 ] && [ $loadfrom == database ]
+  then
+  cp $sqltdata places2.sqlite
+  sqltdata=places2.sqlite
+ fi
  ## This line puts FF bookmarks from sqlite3 to an array ##
-
  dbarray=( $(sqlite3 -list $sqltdata 'select url from moz_places where id in (select fk from moz_bookmarks where parent in ( select "id" from moz_bookmarks where title == "'$favdir'"))'; ))
 fi
-cd  $dl_folder || exit
 
 ## Let youtube-dl do the work  and download brandnew videos ##
+cd $dl_folder || exit
+if [ $aria == 1 ]
+then
+ for i in "${dbarray[@]}"; do
+youtube-dl --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" --external-downloader aria2c --external-downloader-args '-c -j 3 -x 3 -s 3 -k 1M' "$i"
+ done
+ else
+ for i in "${dbarray[@]}"; do
+youtube-dl --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
+ done
+fi
 
-for i in "${dbarray[@]}"; do
-youtube-dl $aria2 --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
-# echo $i
-done
-
-## optional ##
+## optional show downloadfolder ##
 #dolphin $dl_folder
