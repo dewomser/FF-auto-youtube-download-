09cd128d1db22c9a842e025a943cde744eb080a7
dewomser <webmaster@untergang.de>
2019-12-30 14:13:16 +0100

    A notifier for KDE/Plasma.  Shows newly  downloaded videoclips and examples for crontab

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
-
-## This Shellscript downloads all fresh videos from array or firefox bookmark folder with youtube playlists. ##
+## This Shellscript downloads all fresh videos from a firefox bookmark folder. ##
 
 ## Update your youtube-dl ! ##
-# "youtube-dl -U" 
+youtube-dl -U 
 ## or 
 # pip install --upgrade youtube-dl
 sleep 1
-#zaehl=0
-
+zaehl=0
 ## Variables ##
-## ----------important-------------
-## Download folder make it. mkdir ~/Downloads/youtube-dl ##
-dl_folder=~/Downloads/youtube-dl
-## Save all your Youtube playlists in FF bookmark (bestof) ##
-favdir="bestof"
-## ------------ may help----------
+
+## Save all your Youtube playlists in favdir (amp3) ##
+favdir="amp3"
 ## Firefox running ?
+# ffon=0; ps -ef|grep firefox|grep -v grep && ffon=1
 ffon=0; pgrep firefox && ffon=1
-## Firefox default Profil
-## only if you not using the default FF-profile, this line must be edited ## 
-# ffprofile=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=//)
+## Download folder ##
+dl_folder=~/Downloads/youtube-dl
 ## Date= yesterday ##
 datum=$(date -d "1 day ago" '+%Y%m%d')
-## Max. videos download in each playlist ##
+## Max. videos / datum to each playlist ##
 perday=4
 ## if aria2 is installed example : ##
 # aria2='--external-downloader aria2c  --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"'
 aria2=''
-## load from "database" or "array". database == firefox SQLITE 
+## load from "database" or "array".
 loadfrom=database
 ## firefox database
 sqltdata=places.sqlite
 
 ## Start ##
+
 cd ~/.mozilla/firefox/*default* || exit
 
 if [ $ffon == 1 ] && [ $loadfrom == database ]
@@ -41,6 +37,7 @@ then
 cp $sqltdata places2.sqlite
 sqltdata=places2.sqlite
 fi
+
 ## only edit dbarray test-content if you want to use it
 
 if [ $loadfrom == array ]
@@ -49,8 +46,8 @@ then
  "https://www.youtube.com/user/GalileoOffiziell/videos"
  "https://www.youtube.com/user/BibisBeautyPalace/videos?sort=dd&shelf_id=1&view=0"
  "https://www.youtube.com/channel/UC53bIpnef1pwAx69ERmmOLA"
-)
- ##
+ )
+##
 ##--------------------Do not edit below this line ! -----------------------
 ##
  
@@ -62,18 +59,30 @@ fi
 cd  $dl_folder || exit
 
 ## Let youtube-dl do the work  and download brandnew videos ##
-## Wenn die Variable zaehl aktiviert ist, kann der parameter eingebaut werden.
-##--download-archive $dl_folder/archive/archive-$zaehl.txt und ((zaehl++))
 
 for i in "${dbarray[@]}"; do
-youtube-dl $aria2 --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
+((zaehl++))
+youtube-dl $aria2 --download-archive $dl_folder/archive/archive-$zaehl.txt --dateafter "$datum" --playlist-end "$perday" --max-downloads "$perday" "$i"
 # echo $i
 done
+##
+##--------------------Do not edit above this line ! -----------------------
+##
 
+## optional after all Downloads ##
 
-## optional ##
+## Feedback if this script is started from crontab
 
-# dolphin $dl_folder
-## send-notify ##
-# f=$(find $dl_folder -mtime 0,2 -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)') && notify-send "Neue Videos": "$f" --icon=video-x-generic
+## Open folder in dolphin KDE
+#dolphin $dl_folder
+## Notifier KDE
+#f=$(find $dl_folder -mtime 0,2 -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)') && notify-send "Neue Videos": "$f" --icon=video-x-generic
+
+## Notifier KDE in nice
+# This part of the is from now on a separated script. To get in this repository
+
+# f=$(find $dl_folder -mtime 0 -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)')
+# folder1=$(echo $dl_folder|sed "s/\//\\\\\//g")
+# f1=$(echo $f|sed "s/$folder1/\n /g")
+# notify-send "Neue Videos": "$f1" --icon=video-x-generic
 
