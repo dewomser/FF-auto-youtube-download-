95e05eabf3357c12259f99a88610e30e0e9d150f
dewomser <webmaster@untergang.de>
2020-03-18 16:47:05 +0100

    generalüberholte yt-dl-send-notify.sh

--- yt-dl-send-notify.sh
+++ yt-dl-send-notify.sh
@@ -3,16 +3,14 @@
 #This schows new Viedoclips. Script is tested with Kubuntu 19.10 and uses notify-send Popup-Window
 #dl_folder should be the same as in youtube-dl.sh
 #if you use the default,you need not edit this script.
-#If you want to be noticed regulary there is a line for your user crontab
+#If you want to be remembered regulary there is a line for your user crontab
 
 #0 *  * * *  XDG_RUNTIME_DIR=/run/user/$(id -u) /home/foo/bin/yt-dl-send-notify.sh
 
 dl_folder=~/Downloads/youtube-dl/
 f=$(find $dl_folder -mtime 0 -type f -regex '.*\.\(mkv\|mp4\|wmv\|flv\|webm\|mov\)')
+folder1="$(echo $dl_folder|sed "s/\//\\\\\//g")"
+sedarg="sed s/$folder1/\n/g"
+f=$(echo "$f"|$sedarg)
+notify-send "Neue Videos": "$f" --icon=video-x-generic
 
-## comment f1=$f and uncomment next 2 lines. Reason,these lines are not shellcheck compatible.
-#folder1=$(echo $dl_folder|sed "s/\//\\\\\//g")
-#f1=$(echo "$f"|sed "s/$folder1/\n/g")
-f1=$f
-
-notify-send "Neue Videos": "$f1" --icon=video-x-generic
