41d37e9aaecd125cca9f4b6d98dd9ffe9314caf8
dewomser <webmaster@untergang.de>
2020-10-10 07:46:59 +0200

    Shows linked video-clip-folder in popup

--- yt-dl-send-notify.sh
+++ yt-dl-send-notify.sh
@@ -3,6 +3,7 @@
 #This schows new Viedoclips. Script is tested with Kubuntu 19.10 and uses notify-send Popup-Window
 #dl_folder should be the same as in youtube-dl.sh
 #if you use the default,you need not edit this script.
+#if nothing pops up thre is propably no new Video in th e last 4 hours.
 #If you want to be remembered regulary there is a line for your user crontab
 
 #0 *  * * *  XDG_RUNTIME_DIR=/run/user/$(id -u) /home/foo/bin/yt-dl-send-notify.sh
@@ -14,6 +15,8 @@ then exit
 else
 folder1="$(echo $dl_folder|sed "s/\//\\\\\//g")"
 sedarg="sed s/$folder1/\n/g"
-f=$(echo "$f"|$sedarg)
-notify-send "Neue Videos": "$f" --icon=video-x-generic
+f1=$(echo "$f"|$sedarg)
+
+notify-send --icon=video-x-generic "$f1": "Open folder: <a href='$dl_folder'>clips</a>"
+
 fi
